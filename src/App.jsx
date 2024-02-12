189f9254050c44a53e8c3bf331cb91f26268a4d7
Ana Filipa <anafilipa737@gmail.com>
2024-02-12 18:31:07 +0000

    iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import "./App.css";
 import foodsJson from "./foods.json";
 import React, { useState } from 'react';
+import FoodBox from "./components/FoodBox";
  
 
 
 function App() {
- const [items, setItems] = useState(foodsJson)
+ const [foods, setFoods] = useState(foodsJson)
 
-//  useState declares a "state variable" called item in this case.
+//  useState declares a "state variable" called foods in this case.
 // The only argument we pass to the useState() hook is the initial state
 // the useState returns the current state and a function for updating it
 
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <FoodBox food={ {
+      name: "Orange",
+      calories: 85,
+      image: "https://i.imgur.com/abKGOcv.jpg",
+      servings: 1
+    } } 
+    />
     </div>
   );
 }
