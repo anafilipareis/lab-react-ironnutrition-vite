9b791393fd727555c64c05a42cfd3590320337f5
Ana Filipa <anafilipa737@gmail.com>
2024-02-13 15:26:13 +0000

    iteration 5 - not showing up the form

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,30 @@ import "./App.css";
 import foodsJson from "./foods.json";
 import React, { useState } from 'react';
 import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
  
 
 
 function App() {
+//  useState declares a "state variable" called foods in this case.
+// The only argument we pass to the useState() hook is the initial state
+// the useState returns the current state and a function for updating it
  const [foods, setFoods] = useState(foodsJson);
  
-
- const deleteFood = foodId => {
+// Delete Food
+  const deleteFood = foodId => {
   const filteredFoods = foods.filter(food => {
     return food.id !== foodId;
   });
   setFoods(filteredFoods);
  };
 
-//  useState declares a "state variable" called foods in this case.
-// The only argument we pass to the useState() hook is the initial state
-// the useState returns the current state and a function for updating it
+// Add New Food
+const addNewFood = newFood => {
+  const updateFood = [...foods, newFood];
+  setFoods(updateFood);
+};
+
 
   return (
     <div className="App">
@@ -26,7 +33,9 @@ function App() {
       {foods.map((food) => (
       <FoodBox key={food.id} food={food} onDelete={deleteFood} />
       // pass the deleteFood function as a prop to the FoodBox component
-    ))}    
+    ))}
+      <AddFoodForm addFood={addNewFood} />
+
     </div>
   );
 }
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,78 @@
 // Your code here
+
+import { useState } from "react";
+ 
+function AddFoodForm(props) {
+  // declare variavle states
+  const [name, setName] = useState(""); 
+  const [image, setImage] = useState("");
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+
+  // Add an onChange event to each input element and create a handler function for each input
+  const handleNameInput = (e) => setName(e.target.value);
+  const handleImageInput = (e) => setImage(e.target.value);
+  const handleCaloriesInput = (e) => setCalories(e.target.value);
+  const handleServingsInput = (e) => setServings(e.target.value);
+
+  // Prevent Page Reload. Whitout this: If you click on the submit button, you’ll notice the entire app reloading and starting from scratch. This means that all of the state kept in components is lost, wiped out!
+  // To prevent this we have to add an onSubmit event and a handler function to handles the form submission. This way we’ll be able to prevent the default behavior and also access the data that the user entered
+  // The function handleSubmit will be called every time the form is submitted. 
+  const handleSubmit = (e) => {    
+    e.preventDefault(); // we use e.preventDefault() to cancel the default behavior of the form, the page reloads. 
+    const newFood = { name, image, calories, servings }; // After preventing the page from reloading, we access the input data stored in the state (state variables name, etc.).
+ 
+    console.log("Submitted: ", newFood);
+    // Add addFood --> from App.jsx
+    props.addFood(newFood);
+
+    // Reset the state
+    setName("");
+    setImage("");
+    setCalories(0);
+    setServings(0);
+  }
+ 
+  return (
+    <div className="AddFood">
+      <h4>Add Food</h4>
+      <form onSubmit={handleSubmit}>  {/*   <== ADD EVENT */}
+        <label>Name: </label>
+        <input 
+            type="text" 
+            name="name" 
+            value={name} 
+            onChange={handleNameInput}
+        />
+        
+        <label>Image: </label>
+        <input 
+            type="text" 
+            name="image" 
+            value={image} 
+            onChange={handleImageInput}
+        />
+  
+        <label>Calories: </label>
+        <input 
+            type="number" 
+            name="calories" 
+            value={calories} 
+            onChange={handleCaloriesInput}
+        />
+  
+        <label>Servings: </label>
+        <input 
+            type="number" 
+            name="servings" 
+            checked={servings} 
+            onChange={handleServingsInput}
+        />
+        
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+}
+ 
+export default AddFoodForm;
